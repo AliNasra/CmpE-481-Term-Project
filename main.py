e55053f856c5bd2fd06253a08998d81af9d36505
sinemKocoglu <sinemkocoglu560@gmail.com>
2023-11-26 01:39:27 +0300

    updated svm

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.model_selection import RandomizedSearchCV
+import numpy as np
 
 
 def preprocess(X, y):
@@ -35,23 +36,88 @@ def preprocess(X, y):
     
     return X
 
+# hyper parameter tuning for svm
+def param_tuning_svm(X_train, y_train):
+    X_train_t , X_val, y_train_t, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
+    X_train_t_pre = preprocess(X_train_t, y_train_t)  # 0.8 of the training set 
 
-def apply_svm(X_train_t, y_train_t, X_val, y_val, X_test, y_test):
-    clf = SVC(kernel="linear", C=1.0)
-    clf.fit(X_train_t, y_train_t)
+    param_grid = {
+    'kernel': ['linear','poly','rbf','sigmoid'],
+    'C':  [0.01, 0.1, 1, 10, 100],
+    'gamma': ['scale', 'auto', 1, 0.1],
+    }
+    """
+    'coef0':[0.0, 0.1, 0.2, 0.3],
+    'degree':[2, 3, 4, 5]
+    """
+
+    clf = SVC()
+
+    random_search = RandomizedSearchCV(estimator=clf, param_distributions=param_grid, n_iter=5, cv=5, scoring='accuracy', random_state=42, n_jobs=-1)
+    random_search.fit(X_train_t_pre, y_train_t)
+
+    results = random_search.cv_results_
+
+    print("Best parameters : ", random_search.best_params_)
+    print("Average Score for Validation Set : {:.2f}".format(np.mean(results['mean_test_score'])))
+    print("Standard Deviation of Scores for Validation Set: {:.2f}".format(np.std(results['mean_test_score'])))
 
     # For validation set
-    y_pred_val = clf.predict(X_val)
+    eliminated_columns = [feature for feature in X_val.columns if feature not in X_train_t_pre.columns ]
+    X_val = X_val.drop(eliminated_columns, axis=1)
+
+    y_pred_val = random_search.predict(X_val)
     accuracy_val = accuracy_score(y_val, y_pred_val)
+    print("Accuracy for Validation Set: {:.2f}%".format(accuracy_val * 100))  
+    #val_report = classification_report(y_val, y_pred_val)
+    #print(f"Classification report for validation set : {val_report}")
 
-    # For test set
-    y_pred_test = clf.predict(X_test)
-    accuracy_test = accuracy_score(y_test, y_pred_test)
+    # Best Parameters
+    best_kernel = random_search.best_params_['kernel']
+    best_params = {
+    'kernel': best_kernel,
+    'C':  random_search.best_params_['C'],
+    'gamma': 'scale',
+    'coef0':0.0,
+    'degree':3
+    }
 
-    test_report = classification_report(y_test, y_pred_test)
-
-    return accuracy_val, accuracy_test, test_report
+    # gamma exists if best kernel is rbf or poly or sigmoid
+    # coef0 exists if best kernel is poly or sigmoid
+    # degree exists if best kernel is poly
+    if best_kernel!='linear':   # might be rbf or poly or sigmoid
+        best_params['gamma'] = random_search.best_params_['gamma']    
+    else:
+        return best_params
     
+    if best_kernel != 'rbf':   # might be poly or sigmoid
+        best_params['coef0'] = random_search.best_params_['coef0']   
+    else:
+        return best_params  
+         
+    if best_kernel != 'sigmoid':   # is poly
+        best_params['degree'] = random_search.best_params_['degree']   
+    else:
+        return best_params
+    
+
+# retrain model with the whole train set and predict on test set
+def apply_svm(X_train, X_test, y_train, y_test):
+    eliminated_columns = [feature for feature in X_test.columns if feature not in X_train.columns ]
+    X_test = X_test.drop(eliminated_columns, axis=1)
+
+    params = param_tuning_svm(X_train, y_train)
+
+    X_train_pre = preprocess(X_train, y_train)  
+
+    clf = SVC(kernel=params['kernel'], C=params['C'], gamma=params['gamma'])#, coef0=params['coef0'], degree=params['degree']) 
+    clf.fit(X_train_pre, y_train)
+    y_pred_test = clf.predict(X_test)
+
+    accuracy_test = accuracy_score(y_test, y_pred_test)
+    print("Accuracy for Test Set: {:.2f}%".format(accuracy_test * 100))  
+    #test_report = classification_report(y_test, y_pred_test)
+    #print(f"Classification report for test set : {test_report}")
 
 
 data = pd.read_csv('star_classification.csv', index_col=0)
@@ -67,14 +133,6 @@ original_features = X.columns.tolist()
 
 ## Dividing the data by 0.8*0.8 - 0.8*0.2 - 0.2 ratio (training - validation - testing)
 X_train , X_test, y_train, y_test = train_test_split(X, y_numeric, test_size=0.2, random_state=0)
-X_train_t , X_val, y_train_t, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
-
-X_pre = preprocess(X_train, y_train)    # whole training set or 0.8 of the training set ???
-
-"""
-svm_accuracy_val, svm_accuracy_test, svm_test_report = apply_svm(X_train_t, y_train_t, X_val, y_val, X_test, y_test)
-
-print(f"Accuracy for validation set using svm: {svm_accuracy_val}")
-print(f"Accuracy for test set using svm: {svm_accuracy_test}")
-print(f"Classification report for test set using svm: {svm_test_report}")
-"""
+  
+print("SVM")
+apply_svm(X_train, X_test, y_train, y_test)
